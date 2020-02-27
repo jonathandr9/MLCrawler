f981b0ee35de8b770de7be344bad8bce2216ff19
Jonathan <jonathandias96@gmail.com>
2020-02-27 16:25:18 -0300

    Include more tests and readme

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
 # MLCrawler
 
-Api para busca de produtos no site Mercado Livre. Construido em Node.Js.
+Api para busca de produtos no site Mercado Livre. Construido em Node.Js com Express.
 
+## Iniciar a aplicação
 
+Navegar até a raiz do projeto e executar o seguinte comando para baixar as dependências do projeto.
+
+```
+#baixar as dependências do projeto
+npm install
+
+#executar o projeto
+npm start
+```
+
+## Requisição
+
+Deve ser chamado o método findProds, com a seguinte estrutura aprensentada abaixo
+
+```bash
+curl --location --request POST 'http://localhost:3000/findProds' \
+--header 'Content-Type: application/json' \
+--data-raw '{ 
+	"search": "bicicleta",
+    "limit": 500 
+}'
+```
+> O dados seram retornados com a seguinte estrutura
+![result](https://i.imgur.com/fqgep3r.png);
+
+## Teste automatizados
+
+Foi utilizado a ferramenta Jest para a execução dos testes automatizados. Para rodar os testes bastar executar o seguinte comando:
+
+```bash
+    npm test
+```
+
+> Após a finalização dos teste a seguinte saida deve ser apresentada
+
+![terminal](https://i.imgur.com/ELDeg7p.png);
--- __tests__/integrations/searchProducts.test.js
+++ __tests__/integrations/searchProducts.test.js
@@ -5,23 +5,97 @@ const request = require('supertest')
 describe("Search Products", () =>{
 
     beforeEach(() => {
-        jest.setTimeout(10000);
+        jest.setTimeout(100000);
       });
 
-    it("Should returns list of produts", async ()=>{
+    it("Should returns 200", async ()=>{
         
         let find = { 
             search: "relogio", // termo usado na busca 
-            limit: 120 // número de registros retornados 
+            limit: 1 // número de registros retornados 
+        };
+
+        const response = await request(app)  
+                        .post('/findProds')
+                        .send(find);        
+    
+        expect(response.status).toBe(200);
+
+    });
+
+    it("Should returns array legth 135", async ()=>{
+        
+        let find = { 
+            search: "bicicleta", // termo usado na busca 
+            limit: 135 // número de registros retornados 
         };
 
         const response = await request(app)  
                         .post('/findProds')
                         .send(find);
-
-        console.log(response.body.length);
     
-        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(135);
 
     });
+
+    it("Should returns array legth 500", async ()=>{
+        
+        let find = { 
+            search: "bicicleta", // termo usado na busca 
+            limit: 500 // número de registros retornados 
+        };
+
+        const response = await request(app)  
+                        .post('/findProds')
+                        .send(find);
+        
+        
+    
+        expect(response.body.length).toBe(500);
+
+    });
+
+
+    it("Should returns status 404", async ()=>{
+        
+        let find = { 
+            search: "safdffvfsdvcsdçlkvjndsvdljnvdjkvndlvkjbldhdbjkg", // termo usado na busca 
+            limit: 10 // número de registros retornados 
+        };
+
+        const response = await request(app)  
+                        .post('/findProds')
+                        .send(find);
+    
+        expect(response.status).toBe(404);
+
+    });
+
+    it("Should does not return null name, link and price", async ()=>{
+        
+        let find = { 
+            search: "relogio", // termo usado na busca 
+            limit: 500 // número de registros retornados 
+        };
+
+        let nullReturns = false;
+
+        const response = await request(app)  
+                        .post('/findProds')
+                        .send(find);
+
+        response.body.forEach((item) =>{
+
+            if(item.name === null || item.links === null || item.price === null){
+               
+                nullReturns = true;
+            }            
+        });
+    
+        expect(nullReturns).toBe(false);
+
+    });
+
+
+
 });
